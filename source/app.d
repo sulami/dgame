61f143751bb74eb6ddbd098dc324c73f64676f89
Robin Schroer <sulamiification@gmail.com>
2014-08-26 23:43:50 +0200

    draw some random rectangle in the wrote location

--- source/app.d
+++ source/app.d
@@ -1,4 +1,3 @@
-import core.thread;
 import std.stdio;
 
 import derelict.opengl3.gl3;
@@ -12,12 +11,27 @@ int main(string[] args)
 
     /* create a window and a renderer to use */
     auto window = SDL_CreateWindow("DGame", SDL_WINDOWPOS_UNDEFINED,
-                                   SDL_WINDOWPOS_UNDEFINED, 800, 600, 0);
+                                   SDL_WINDOWPOS_UNDEFINED, 800, 600,
+                                   SDL_WINDOW_OPENGL);
     auto renderer = SDL_CreateRenderer(window, -1, 0);
+    auto context = SDL_GL_CreateContext(window);
 
-    Thread.sleep(dur!("seconds")(5));
+    /* clear the screen, i want it to be black */
+    glClearColor(0 ,0 ,0 ,1);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-    DerelictGL3.reload();
+    /* draw some random rect */
+    SDL_Rect *r = new SDL_Rect(0, 0, 10, 10);
+    SDL_RenderFillRect(renderer, r);
+
+    /* swap buffers */
+    SDL_GL_SwapWindow(window);
+
+    /* wait briefly to inspect the results */
+    SDL_Delay(1000);
+
+    /* clean up */
+    SDL_Quit();
 
     return 0;
 }
