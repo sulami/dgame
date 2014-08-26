f4d8605bc0e38f24eea73234918cac42b19770f4
Robin Schroer <sulamiification@gmail.com>
2014-08-26 23:07:18 +0200

    open a window to render in

--- source/app.d
+++ source/app.d
@@ -1,7 +1,24 @@
+import core.thread;
 import std.stdio;
+
+import derelict.opengl3.gl3;
+import derelict.sdl2.sdl;
 
 int main(string[] args)
 {
+    /* load whatever we need */
+    DerelictGL3.load();
+    DerelictSDL2.load();
+
+    /* create a window and a renderer to use */
+    auto window = SDL_CreateWindow("DGame", SDL_WINDOWPOS_UNDEFINED,
+                                   SDL_WINDOWPOS_UNDEFINED, 800, 600, 0);
+    auto renderer = SDL_CreateRenderer(window, -1, 0);
+
+    Thread.sleep(dur!("seconds")(5));
+
+    DerelictGL3.reload();
+
     return 0;
 }
 
