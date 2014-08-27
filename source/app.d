c0714a1dea5b000e8d0cb174d7f41aa037c1a309
Robin Schroer <sulamiification@gmail.com>
2014-08-27 11:44:34 +0200

    add a renderrect class to wrap/manage rectangles rendered to the screen

--- source/app.d
+++ source/app.d
@@ -3,19 +3,58 @@ import std.stdio;
 import derelict.opengl3.gl3;
 import derelict.sdl2.sdl;
 
+class RenderRect
+{
+    int x, y;
+    uint w, h;
+    ubyte r, g, b, a;
+    SDL_Renderer *renderer;
+
+    this(SDL_Renderer *renderer, int x, int y, uint w, uint h, ubyte r,
+         ubyte g, ubyte b, ubyte a)
+    {
+        this.renderer = renderer;
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = a;
+
+        SDL_SetRenderDrawColor(this.renderer, this.r, this.g, this.b, this.a);
+        SDL_Rect *rect = new SDL_Rect(this.x, this.y, this.w, this.h);
+        SDL_RenderFillRect(this.renderer, rect);
+    }
+
+    void move(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+
+        SDL_SetRenderDrawColor(this.renderer, this.r, this.g, this.b, this.a);
+        SDL_Rect *rect = new SDL_Rect(this.x, this.y, this.w, this.h);
+        SDL_RenderFillRect(this.renderer, rect);
+    }
+}
+
 int main(string[] args)
 {
     /* load whatever we need */
     DerelictGL3.load();
     DerelictSDL2.load();
 
+    const int ww = 800, wh = 600;
+
     /* create a window and a renderer to use */
     auto window = SDL_CreateWindow("DGame", SDL_WINDOWPOS_UNDEFINED,
-                                   SDL_WINDOWPOS_UNDEFINED, 800, 600,
+                                   SDL_WINDOWPOS_UNDEFINED, ww, wh,
                                    SDL_WINDOW_OPENGL);
     auto renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    int x, y;
+    RenderRect rect = new RenderRect(renderer, 10, 10, 50, 50, 255, 0, 0,
+                                        255);
 
     /* do stuff until we kill the window */
     while (true) {
@@ -29,9 +68,7 @@ int main(string[] args)
         SDL_RenderClear(renderer);
 
         /* draw some random rect */
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-        SDL_Rect *r = new SDL_Rect(x++, y++, 100, 100);
-        SDL_RenderFillRect(renderer, r);
+        rect.move(rect.x + 1, rect.y + 1);
 
         /* swap buffers */
         SDL_RenderPresent(renderer);
