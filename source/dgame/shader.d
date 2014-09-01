2606969b193b36d1cae895445f3f52c0af4da666
Robin Schroer <sulamiification@gmail.com>
2014-09-01 20:31:46 +0200

    make shader loading use exceptions to fail on compilation errors

--- source/dgame/shader.d
+++ source/dgame/shader.d
@@ -3,6 +3,7 @@ module shader;
 import std.conv;
 import std.file;
 import std.stdio;
+import std.string;
 
 import derelict.opengl3.gl3;
 
@@ -15,30 +16,21 @@ class Shader
         glDeleteShader(shader);
     }
 
-    bool loadShader(GLenum type, string path)
+    void loadShader(GLenum type, string path)
     {
-        try
-        {
-            string code = readText(path);
-            const char *ptr = code.ptr;
-            int len = to!int(code.length);
+        string code = readText(path);
+        const char *ptr = code.ptr;
+        int len = to!int(code.length);
 
-            shader = glCreateShader(type);
-            glShaderSource(shader, 1 ,&ptr, &len);
-            glCompileShader(shader);
+        shader = glCreateShader(type);
+        glShaderSource(shader, 1 ,&ptr, &len);
+        glCompileShader(shader);
 
-            int result;
-            glGetShaderiv(shader, GL_COMPILE_STATUS, &result);
-            if (!result)
-                writeln("Failed to compile shader: ", shader);
-
-            return true;
-        }
-        catch (Exception e)
-        {
-            writeln("Failed to load shader: \n", e);
-            return false;
-        }
+        int result;
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &result);
+        if (!result)
+            throw new Exception(
+                format("Failed to compile shader %s from %s", shader, path));
     }
 }
 
