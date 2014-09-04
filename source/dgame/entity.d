8d07f7649ca1893d4e202502ce4fbf08fa0badb2
Robin Schroer <sulamiification@gmail.com>
2014-09-04 18:54:50 +0200

    switch from hardcoded cube models to obj files

--- source/dgame/entity.d
+++ source/dgame/entity.d
@@ -1,5 +1,8 @@
 module entity;
 
+import std.stdio;
+import std.string;
+
 import derelict.opengl3.gl3;
 import gl3n.linalg;
 
@@ -12,26 +15,27 @@ class Entity
     mat4 Model, MVP;
     GLuint MatrixID;
     GLuint vertexbuffer, uvbuffer;
-    GLfloat g_vertex_buffer_data[];
-    GLfloat g_uv_buffer_data[];
     Texture texture;
+    vec3 vertexBuffer[];
+    vec2 uvBuffer[];
+    vec3 normalBuffer[];
 
-    this(Display d, GLfloat vbuf[], Texture t, GLfloat ubuf[])
+    this(Display d, Texture t, string path)
     {
+        loadObj(path);
+
         display = d;
 
         MatrixID = display.program.getUniformLocation("MVP");
         Model = mat4.identity();
 
-        g_vertex_buffer_data = vbuf;
-        g_uv_buffer_data = ubuf;
         texture = t;
 
         glGenBuffers(1, &vertexbuffer);
         glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
         glBufferData(GL_ARRAY_BUFFER,
-                     g_vertex_buffer_data.length * GLfloat.sizeof,
-                     g_vertex_buffer_data.ptr, GL_STATIC_DRAW);
+                     vertexBuffer.length * vec3.sizeof,
+                     vertexBuffer.ptr, GL_STATIC_DRAW);
 
         glGenBuffers(1, &uvbuffer);
         glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
@@ -39,11 +43,68 @@ class Entity
         d.addEntity(this);
     }
 
+    private void loadObj(string path)
+    {
+        string text[];
+        uint vertexIndices[];
+        uint uvIndices[];
+        uint normalIndices[];
+        vec3 tmp_vertices[];
+        vec2 tmp_uvs[];
+        vec3 tmp_normals[];
+
+        File obj = File(path, "r");
+        while (!obj.eof())
+            text ~= chomp(obj.readln());
+        obj.close();
+
+        for (int i = 0; i < text.length; i++) {
+            string l[] = text[i].split(" ");
+
+            if (l.length == 0)
+                continue;
+
+            switch (l[0]) {
+                case "v":
+                    tmp_vertices ~= vec3(to!float(l[1]), to!float(l[2]),
+                                         to!float(l[3]));
+                    break;
+                case "vt":
+                    tmp_uvs ~= vec2(to!float(l[1]), to!float(l[2]));
+                    break;
+                case "vn":
+                    tmp_normals ~= vec3(to!float(l[1]), to!float(l[2]),
+                                        to!float(l[3]));
+                    break;
+                case "f":
+                    foreach (set; l[1..4]) {
+                        string tup[3] = set.split("/");
+                        vertexIndices ~= to!uint(tup[0]);
+                        uvIndices ~= to!uint(tup[1]);
+                        normalIndices ~= to!uint(tup[2]);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        for (int i = 0; i < vertexIndices.length; i++) {
+            uint vertexIndex = vertexIndices[i];
+            uint uvIndex = uvIndices[i];
+            uint normalIndex = normalIndices[i];
+
+            vertexBuffer ~= tmp_vertices[vertexIndex - 1];
+            uvBuffer ~= tmp_uvs[uvIndex - 1];
+            normalBuffer ~= tmp_normals[normalIndex - 1];
+        }
+    }
+
     void render()
     {
         glBufferData(GL_ARRAY_BUFFER,
-                    g_uv_buffer_data.length * GLfloat.sizeof,
-                    g_uv_buffer_data.ptr, GL_STATIC_DRAW);
+                     uvBuffer.length * vec2.sizeof,
+                     uvBuffer.ptr, GL_STATIC_DRAW);
 
         MVP = display.Perspective * display.View * Model;
         glUniformMatrix4fv(MatrixID, 1, GL_TRUE, &MVP[0][0]);
@@ -60,7 +121,7 @@ class Entity
         glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
         glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, cast(void *)0);
 
-        glDrawArrays(GL_TRIANGLES, 0, cast(int)g_vertex_buffer_data.length);
+        glDrawArrays(GL_TRIANGLES, 0, cast(int)vertexBuffer.length);
 
         glDisableVertexAttribArray(0);
         glDisableVertexAttribArray(1);
