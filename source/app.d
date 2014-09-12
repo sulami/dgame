c8dff26eb1184fe13b3c525e79e1b2c6ca19e7f1
Robin Schroer <sulamiification@gmail.com>
2014-09-12 20:17:58 +0200

    change rendered objects from two cubes to a monkey

--- source/app.d
+++ source/app.d
@@ -5,16 +5,16 @@ int main(string[] args)
 {
     Display display = new Display(1024, 768, 45);
 
-    Entity cube = new Entity(display, "cube.obj", "wurfel.png");
-    Entity cube2 = new Entity(display, "cube.obj", "tex2.png");
-    /* Entity monkey = new Entity(display, "monkeyhd.obj", "tex2.png"); */
+    /* Entity cube = new Entity(display, "cube.obj", "wurfel.png"); */
+    /* Entity cube2 = new Entity(display, "cube.obj", "tex2.png"); */
+    Entity monkey = new Entity(display, "monkeyhd.obj", "tex.png");
 
-    cube2.move(0f,-2.1,0f);
+    /* cube2.move(0f,-2.1,0f); */
 
     while (display.event()) {
-        cube.rotate(0f, .05, 0f);
-        cube2.rotate(0f, -.02, 0f);
-        /* monkey.rotate(0f, -.02, 0f); */
+        /* cube.rotate(0f, .05, 0f); */
+        /* cube2.rotate(0f, -.02, 0f); */
+        monkey.rotate(0f, -.02, 0f);
         display.render();
     }
 
